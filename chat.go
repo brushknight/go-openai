fa27aff8f0d76791071916b091148e72ec451f46
Grigorii Merkushev <brushknight@gmail.com>
2024-06-12 11:55:35 +0200

    add extra params for chat completion

--- chat.go
+++ chat.go
@@ -189,6 +189,8 @@ type ChatCompletionRequest struct {
 	MaxTokens        int                           `json:"max_tokens,omitempty"`
 	Temperature      float32                       `json:"temperature,omitempty"`
 	TopP             float32                       `json:"top_p,omitempty"`
+	MinP             float32                       `json:"min_p,omitempty"`
+	TopK             float32                       `json:"top_k,omitempty"`
 	N                int                           `json:"n,omitempty"`
 	Stream           bool                          `json:"stream,omitempty"`
 	Stop             []string                      `json:"stop,omitempty"`
@@ -196,6 +198,7 @@ type ChatCompletionRequest struct {
 	ResponseFormat   *ChatCompletionResponseFormat `json:"response_format,omitempty"`
 	Seed             *int                          `json:"seed,omitempty"`
 	FrequencyPenalty float32                       `json:"frequency_penalty,omitempty"`
+	RepeatPenalty    float32                       `json:"repeat_penalty,omitempty"`
 	// LogitBias is must be a token id string (specified by their token ID in the tokenizer), not a word string.
 	// incorrect: `"logit_bias":{"You": 6}`, correct: `"logit_bias":{"1639": 6}`
 	// refs: https://platform.openai.com/docs/api-reference/chat/create#chat/create-logit_bias
